a17f391ea333cc78090ab4acc19d803928a02c09
akshay <akshayvk4151@gmail.com>
2023-09-30 10:48:15 +0530

    first commit

---
+++ src/openai.js
@@ -0,0 +1,3 @@
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({ apiKey: "sk-oKI1Lfc5Lp8d4J12TpQCnT3BlbkFJcVloBRPEkFUuLvl3acpw" });
+const openai = new OpenAIApi(configuration);
